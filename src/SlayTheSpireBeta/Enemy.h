dc69acf63f325666bb21b5079fa3daf392540a74
Amin Emam <aminemam.w@gmail.com>
2026-06-22 20:24:38 +0330

    Enemy header file added at UML level

--- src/SlayTheSpireBeta/Enemy.h
+++ src/SlayTheSpireBeta/Enemy.h
@@ -1,4 +1,27 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+class Enemy {
+private:
+    int hp;
+    int maxHp;
+    int block = 0;
+
+public:
+
+    void takeDamage(int damage);
+    void addBlock(int damage);
+    // void attack(int damage, Player& p);
+    bool isAlive();
+};
+
+
+
+
+
+
+
+
+
+
 #endif // ENEMY_H
