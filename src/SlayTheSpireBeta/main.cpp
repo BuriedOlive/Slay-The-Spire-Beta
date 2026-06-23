6dde6b360ec628655575ed06343b1062b13911be
Amin Emam <aminemam.w@gmail.com>
2026-06-23 23:06:15 +0330

    Forgor to commit lmao :skull:

--- src/SlayTheSpireBeta/main.cpp
+++ src/SlayTheSpireBeta/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w( BattleManager(Player(50, 6), Enemy(14, 10) ));
     w.show();
     return QApplication::exec();
 }
